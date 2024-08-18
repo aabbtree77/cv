f2541cd0bb9ae327b3f1855533423375e9e0d444
Ramunas Girdziusas <aabbtree77@gmail.com>
2024-08-18 16:59:10 +0300

    Fixed ToggleFont @ medium scr

--- src/components/DX.tsx
+++ src/components/DX.tsx
@@ -2,17 +2,17 @@ import React from "react";
 
 const DX: React.FC = () => {
   return (
-    <div className="container mx-auto flex flex-col gap-4 2xl:gap-8 2xl:w-3/5">
+    <div className="container mx-auto flex flex-col gap-4 2xl:gap-8 lg:w-3/5">
       <div className="text-2xl lg:text-3xl font-bold text-center">Experience</div>
       <div className="text-base lg:text-xl text-justify">
         <p>
-          Recently: indie frontend/full-stack web. HTML, CSS,
+          Recently: frontend/full-stack web. HTML, CSS,
           JavaScript, TypeScript, React, Tailwind, Astro. A bit of: Git, Docker, SQL,
           MongoDB.
         </p>
         <br />
         <p>
-          In the past: hobby embedded software. C (ATmega, avr-gcc), MicroPython (ESP32, MQTT, routers,
+          In the past: embedded software. C (ATmega, avr-gcc), MicroPython (ESP32, MQTT, routers,
           Linux), Go (golibp2p), C++ (OpenGL).
         </p>
         <br />
