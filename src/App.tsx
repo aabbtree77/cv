6bdcef39ae1cde21d32a9be6eb498328a18ad2a8
Ramunas Girdziusas <aabbtree77@gmail.com>
2024-08-18 13:26:29 +0300

    Fixed ToggleFont @ medium scr

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-neutral">
-      <div className="mx-auto flex w-11/12 flex-col items-center gap-8 text-gray-400 lg:w-3/5 lg:gap-24">
+      <div className="mx-auto flex w-11/12 flex-col items-center gap-8 text-gray-400 2xl:w-3/5 2xl:gap-24">
         <Header />
         <DX />
         <Portfolio />
--- src/components/DX.tsx
+++ src/components/DX.tsx
@@ -2,22 +2,22 @@ import React from "react";
 
 const DX: React.FC = () => {
   return (
-    <div className="container mx-auto flex flex-col gap-4 lg:gap-8 lg:w-1/2">
-      <div className="text-2xl lg:text-3xl font-bold text-center">Skills</div>
+    <div className="container mx-auto flex flex-col gap-4 2xl:gap-8 2xl:w-3/5">
+      <div className="text-2xl lg:text-3xl font-bold text-center">Experience</div>
       <div className="text-base lg:text-xl text-justify">
         <p>
-          Recently: Frontend/Full-Stack Indie Web Development. HTML, CSS,
-          JavaScript, TypeScript, React, Tailwind. A bit of: Git, Docker, SQL,
+          Recently: indie frontend/full-stack web. HTML, CSS,
+          JavaScript, TypeScript, React, Tailwind, Astro. A bit of: Git, Docker, SQL,
           MongoDB.
         </p>
         <br />
         <p>
-          In the past: C (ATmega, avr-gcc), MicroPython (ESP32, MQTT, routers,
+          In the past: hobby embedded software. C (ATmega, avr-gcc), MicroPython (ESP32, MQTT, routers,
           Linux), Go (golibp2p), C++ (OpenGL).
         </p>
         <br />
         <p>
-        More distant past: Python (Matplotlib, PyTorch), Matlab, Scilab, LaTeX, linear algebra, calculus.
+        More distant past: academic research in machine learning. Python, Matplotlib, PyTorch, Matlab, Scilab, LaTeX, linear algebra, calculus.
         </p>
       </div>
     </div>
