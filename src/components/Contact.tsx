055328ace7de43a52bb0bdafa0cad6197a6956a8
Ramunas Girdziusas <aabbtree77@gmail.com>
2024-09-23 17:53:14 +0300

    minor update

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -6,13 +6,9 @@ const Contact: React.FC = () => {
       <div className="text-2xl lg:text-3xl font-bold text-center">Contact</div>
       <div>
         <div className="flex flex-col gap-2 text-base lg:text-2xl items-center">
-          <div>+37061042349</div>
 
-          <div>aabbtree77 (at) gmail.com</div>
+          <div className="max-lg:w-min">aabbtree77 (at) gmail.com</div>
 
-          <div className="max-lg:w-min">
-            Viber (&ldquo;Ramūnas Girdziušas&rdquo;)
-          </div>
         </div>
       </div>
     </div>
